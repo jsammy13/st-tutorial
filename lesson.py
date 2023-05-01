038ca5fef19717153b51b43f88347647285585b8
jsammy13 <132211720+jsammy13@users.noreply.github.com>
2023-05-01 19:35:24 +0800

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -1,8 +1,33 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Hello World this is Jo's first app")
 
-st.subheader("Building the first app on the Streamlit platoform")
+st.subheader("Building the first app on the Streamlit platform")
+
+st.text(" Hi everyone, this is just a testing of the new platform to see what it can do")
+
+st.markdown("### This is a. markdown  section of the page")
+
+st.success("Success command")
+st.info("Here is some information")
+st.error("We hit an error")
+st.warning("Here is a warning")
+exp=ZeroDivisionError("Trying to divide by Zero")
+st.exception(exp)
+
+
+st.write("Writing a series of text using st.write")
+st.write("writing a range of numbers using st.write range")
+st.write(range(10)
+
+img=Image.open("streamlit.png")
+st.image(img, width=200)
+         
+
+         
+
+
 
 
 
