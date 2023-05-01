76cd059bf55c4acabe69c2a1b8a7b71d2a98d292
jsammy13 <132211720+jsammy13@users.noreply.github.com>
2023-05-01 20:33:10 +0800

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -39,6 +39,12 @@ if(st.button('Submit')):
          result = name.title()
          st.success(result)
 
+         
+level = st.slider("Select the level", 1, 5)
+st.text('Selected: {}'.format(level))
+
+
+
 
 
 
