f063112a7f3286af32fb2fd5438c0511aa75511a
jsammy13 <132211720+jsammy13@users.noreply.github.com>
2023-05-01 19:50:57 +0800

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -21,7 +21,8 @@ st.write(range(10))
 
 if st.checkbox("Show/Hide"):
          st.text("Showing the widget")
-
+   
+status = st.radio("Select Gender: ", ('Male', 'Female'))
 if (status =='Male'):
          st.success("Male")
 else:
