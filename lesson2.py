d9e2a99347dc942959651ce4bf5712da9a5a7921
jsammy13 <132211720+jsammy13@users.noreply.github.com>
2023-05-01 23:56:10 +0800

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -4,6 +4,31 @@ st.title('Welcome to BMI Calculator')
 
 weight =st.number_input("Enter your weight (in kgs)")
 
-statusn= st.radio('Select you height format: ', ('cm', 'meter', 'feet'))
+status= st.radio('Select you height format: ', ('cm', 'meter', 'feet'))
+
+if(status == 'cm') :
+  height = st.number_input('Centimeters ')
+  
+  try:
+    bmi = weight/((height/100)**2)
+  except:
+    st.text("Enter some value of height in cm")
+    
+elif(status =='meter'):
+  height = st.number_input('Meter ')
+  
+  try:
+    bmi = weight/(height **2)
+  except:
+    st.text("Enter some value of height in m")
+ 
+else:
+  height = st.number_input('Feet ')
+  
+  try:
+    bmi = weight/((height/3.28)**2)
+  except:
+    st.text("Enter some value in height in ft")
+ 
 
 
